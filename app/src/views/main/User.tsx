5d51a93aa4bb5f84af23b5d3605cc785308e7c77
junyoung kim <junyoungkim@junyoungui-MacBookAir.local>
2022-06-15 00:53:51 +0900

    ?

--- app/src/views/main/User.tsx
+++ app/src/views/main/User.tsx
@@ -1,37 +1,71 @@
-import {useState, Component, ReactNode} from 'react';
-import {Grid, GridColumn, GridToolbar} from "@progress/kendo-react-grid";
+import React, {useState, Component, ReactNode} from 'react';
+import {
+    getSelectedState,
+    Grid,
+    GridColumn as Column, GridHeaderSelectionChangeEvent,
+    GridSelectionChangeEvent,
+    GridToolbar
+} from "@progress/kendo-react-grid";
 import '@progress/kendo-theme-default/dist/all.css';
 import BaseModal from '../modals/BaseModal';
-import * as path from 'path';
+import {getter} from "@progress/kendo-data-query";
 const electron = window.require('electron');
 const ipcRenderer = electron.ipcRenderer;
 
+
+
+const DATA_ITEM_KEY: string = "_id";
+const SELECTED_FIELD: string = "selected";
+const idGetter = getter(DATA_ITEM_KEY);
+
+
+interface UserState {
+    data: [];
+    modal : {
+        show : boolean
+    }
+    selectedState: {
+        [id: string]: boolean | number[];
+    };
+}
+
+
 class User extends Component {
-    state = {
-        data: [],
+    state : UserState = {
+        data: ipcRenderer.sendSync("@User/index").data.map((dataItem:object) => {
+            return Object.assign({ selected: false }, dataItem);
+        }),
         modal : {
             show : true
-        }
+        },
+        selectedState : { 1 : true}
     }
+    onSelectionChange = (event: GridSelectionChangeEvent) => {
+        const selectedState = getSelectedState({
+            event,
+            selectedState: this.state.selectedState,
+            dataItemKey: DATA_ITEM_KEY,
+        });
+            console.log(selectedState);
+        this.setState({ selectedState });
+    };
+    onHeaderSelectionChange = (event: GridHeaderSelectionChangeEvent) => {
+        const checkboxElement: any = event.syntheticEvent.target;
+        const checked = checkboxElement.checked;
+        const selectedState = {};
 
+        this.state.data.forEach((item) => {
+            // @ts-ignore
+            selectedState[idGetter(item)] = checked;
+        });
+
+        this.setState({ selectedState });
+    };
     customClick = () => {
         alert("Custom handler in custom toolbar");
     }
     constructor(props : any) {
         super(props);
-        const users = ipcRenderer.sendSync("@User/index");
-
-
-        if(users.success){
-
-            // @ts-ignore
-            this.state.data = users.data;
-            // this.setState({
-            //     data : [{
-            //         user_name : 'test'
-            //     }]
-            // })
-        }
     }
     loadUser(){
         console.log('start load user');
@@ -51,7 +85,20 @@ class User extends Component {
                 style={{
                     height: "400px",
                 }}
-                data={this.state.data}
+                data={this.state.data.map((item:object) => ({
+                    ...item,
+                    [SELECTED_FIELD]: this.state.selectedState[idGetter(item)],
+                }))}
+                dataItemKey={DATA_ITEM_KEY}
+                selectedField={SELECTED_FIELD}
+                selectable={{
+                    enabled: true,
+                    drag: false,
+                    cell: false,
+                    mode: "multiple",
+                }}
+                onSelectionChange={this.onSelectionChange}
+                onHeaderSelectionChange={this.onHeaderSelectionChange}
             >
                      <GridToolbar>
                          <BaseModal
@@ -82,11 +129,12 @@ class User extends Component {
                                     text : 'save',
                                     click : (modal: BaseModal) => {
                                         const userInsert = ipcRenderer.sendSync("@User/insert",modal.getInputValues());
-                                        console.log('return userInsert',userInsert);
+
                                         if(userInsert.success){
-                                            console.log('insert afetr reload');
+
                                             _this.loadUser();
                                             modal.close();
+
                                         }
 
                                     }
@@ -94,8 +142,17 @@ class User extends Component {
                             }
                          />
                      </GridToolbar>
-                <GridColumn field="user_name" title="이름" width="100px" />
-                <GridColumn field="phone_number" title="폰" width="100px" />
+                <Column
+                    field={SELECTED_FIELD}
+                    width="50px"
+                    headerSelectionValue={
+                        this.state.data.findIndex(
+                            (item) => !this.state.selectedState[idGetter(item)]
+                        ) === -1
+                    }
+                />
+                <Column field="user_name" title="이름" width="100px" />
+                <Column field="phone_number" title="폰" width="100px" />
                 {/*<GridColumn field="ProductID" title="ID" width="40px" />*/}
                 {/*<GridColumn field="ProductName" title="Name" width="250px" />*/}
                 {/*<GridColumn field="Category.CategoryName" title="CategoryName" />*/}
