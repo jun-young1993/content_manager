c74ec7a90baeb8a04eb467857cd7c3e818c64258
junyoungkim <juny3738@gmail.com>
2022-11-12 18:27:22 +0900

    gitignore

--- app/public/src/events/ready/createServer.js
+++ app/public/src/events/ready/createServer.js
@@ -2,9 +2,13 @@
 // @ts-ignore
 var express = require('express');
 var app = express();
+var shareApp = express();
 app.use('/thumbnail', require('../../../lib/route/thumbnail'));
 app.use('/play', require('../../../lib/route/play'));
-app.use("/share", require("../../../lib/route/share"));
 app.listen(11101, function () {
     console.log('Example app listening on port 11101!');
+});
+shareApp.use("/share", require("../../../lib/route/share"));
+shareApp.listen(11103, function () {
+    console.log('Example app listening on port 11103!');
 });
--- app/public/src/events/ready/createServer.ts
+++ app/public/src/events/ready/createServer.ts
@@ -3,12 +3,17 @@
 // @ts-ignore
 const express = require('express');
 const app = express();
+const shareApp = express();
 
 
 
 app.use('/thumbnail',require('../../../lib/route/thumbnail'));
 app.use('/play',require('../../../lib/route/play'));
-app.use("/share",require("../../../lib/route/share"));
 app.listen(11101, function () {
     console.log('Example app listening on port 11101!');
+});
+
+shareApp.use("/share",require("../../../lib/route/share"));
+shareApp.listen(11103, function () {
+    console.log('Example app listening on port 11103!');
 });
