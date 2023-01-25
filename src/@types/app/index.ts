ba4f3902fbe0bd6760331136f8eda977960309e6
kim-jun-young <jykim@gemiso.com>
2023-01-25 15:43:02 +0900

    app props added

--- src/@types/app/index.ts
+++ src/@types/app/index.ts
@@ -1,2 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AppState {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AppProps {}
